178517fb93b6edb55905ecf0a11418b99f5b9ef4
IzaKli <93653355+IzaKlis@users.noreply.github.com>
2023-11-21 09:47:08 +0100

    Add files via upload

---
+++ IZA/IOS6/IOS6/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  IOS6
+//
+//  Created by student on 14/11/2023.
+//  Copyright © 2023 me.kozlowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [_informationButton setTitle:NSLocalizedString(@"Information",nil) forState:(UIControlStateNormal)];
+    [_imageWI setImage:[UIImage imageNamed:NSLocalizedString(@"image", nil)]];
+}
+-(IBAction)buttonClick :(id)sender{
+    UIAlertController *alertDialog=[UIAlertController alertControllerWithTitle:NSLocalizedString(@"Information",nil) message:[NSString stringWithFormat:NSLocalizedString(@"The faculty is running %i programs at graduate and ungraduate level.", nil),4] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *defaultAction=[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                        handler:^(UIAlertAction *action){}];
+    [alertDialog addAction:defaultAction];
+    [self presentViewController:alertDialog animated:YES completion:nil];
+    
+}
+@end
